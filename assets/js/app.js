289a20a102438bb6f8feed42604b311880537977
tstrickland88 <77135762+tstrickland88@users.noreply.github.com>
2021-05-13 22:03:48 -0700

    added save high score function

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,7 @@ createquestion()
 function countdown(){
     if (time<=0){ 
         endtimer()
+        failed()
     }else{
         time=time-1 
     Score.innerHTML = time
@@ -68,11 +69,19 @@ function hidefeedback(){
         feedback.innerHTML = "<h3> Correct </h3>"
     }else{
         feedback.innerHTML = "<h3> Incorrect </h3>"
+        time=time-5
     }
    setTimeout(hidefeedback,1000);
+   if(curQuestion==questions.length-1){
+       endquiz()
+   }
+   else{
     curQuestion++
-    createquestion()
+    createquestion() 
+   }
+
 }
+
 
 function endquiz(){
     clearInterval(quiztime)
@@ -86,10 +95,26 @@ function endquiz(){
     </div>
     <div>
     <h3> You Scored ${time} Points</h3>
-
+<p> Enter Your Initials:<input type="text" id="initial" max="4"/> </p> 
         <button class="btn btn-success sel-btn" onclick=" " >
         Add High Score
         </button>
     </div>
     </div>`
 } 
+
+function failed(){
+
+    var interface=document.getElementById("interface")
+    interface.innerHTML = `<div>
+    <div>
+        <h2>
+            You Did Bad! 
+        </h2>
+    </div>
+    <div>
+    <h3> You Ran Out OF Time </h3>
+
+    </div>
+    </div>`
+}
