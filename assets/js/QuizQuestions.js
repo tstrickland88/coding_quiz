1cea3f18de4c5b343b80041c3a97db28e8e1f655
tstrickland88 <77135762+tstrickland88@users.noreply.github.com>
2021-05-18 22:36:19 -0700

    added more questions and answers

--- assets/js/QuizQuestions.js
+++ assets/js/QuizQuestions.js
@@ -1,11 +1,25 @@
 var questions=[{
-    question: "Question 1",
+    question: "What is My Dogs Name?",
     choices: ["Princess","Grumpy Puff","Missy","Hassani"],
     answer: "Princess"
-},{
-    question: "question 2",
-    choices: ["a","b","c","d"],
-    answer: "b"
+},
+
+{
+    question: "What Does Yolo Mean?",
+    choices: ["You Owe Lee One's","You Only Live Once","You Only Lie Once","You Only Leep Once"],
+    answer: "You Only Live Once"
+},
+
+{
+    question: "Who was the lead singer of Nsync",
+    choices: ["Justin Timberlake","Joey Fantone","Lance Bassc","CJ Chasez"],
+    answer: "Justin Timberlake"
+},
+
+{
+question: "What Was the Color of Clifford the Big Red Dog?",
+choices: ["Blue","Yellow","Red","Purple"],
+answer: "Red"
 }
 ]
 
